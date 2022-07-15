6bbe8f396189943a0b10c800e59eb28359666f3b
Will <willchesson@gmail.com>
2022-07-15 18:25:00 -0400

    commit tree 23d8d7a7482157f645467bd6bd524fe1be40743e

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,29 @@
 require_relative './stack'
 
-# your code here
+class MyQueue
+
+    def initialize
+    @s1 = Stack.new
+    @s2 = Stack.new
+    end
+
+    def add(value)
+        #s2 = [2, 1] s1 = []
+        @s1.push(@s2.pop) until @s2.empty?
+
+        #s1 = [1, 2]
+        @s1.push(value) 
+
+        #s1 = [1, 2, 3]
+        @s2.push(@s1.pop) until @s1.empty?
+    end
+
+    def remove
+        @s2.pop
+    end
+
+    def peek
+        @s2.peek
+    end
+
+end
